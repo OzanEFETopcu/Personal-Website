e7e591ac795be583d52bf0e6076ef9e3d0f63927
OzanEFETopcu <ozanefe.topcu@gmail.com>
2023-01-31 15:44:46 +0200

    Updating the projects part

--- client_new/src/Projects.js
+++ client_new/src/Projects.js
@@ -10,34 +10,34 @@ export default function Projects() {
           <div className='container-fluid' id='projectsContainer1'>
 
             <div className='row flex' id='projectsrow1'>
-              <div id='project1_div'>
+              <div className='project_div1'>
+                <div className='text-white p-5'>
+                  <p>This is my first project. This paragraph will be changed later. OOGA BOOGA</p>
+                </div>
                 <div>
                   <img src={Project1} alt='Project1 Image' id='project1_img'/>
-                </div>
-                <div className='text-white'>
-                  <p>This is my first project. This paragraph will be changed later. OOGA BOOGA</p>
                 </div>
               </div>
             </div>
 
             <div className='row flex' id='projectsrow2'>
-              <div id='project1_div'>
+              <div className='project_div2'>
                 <div>
-                  <img src={Project1} alt='Project1 Image' id='project1_img'/>
+                  <img src={Project1} alt='Project2 Image' id='project2_img'/>
                 </div>
-                <div className='text-white'>
+                <div className='text-white p-5'>
                   <p>This is my first project. This paragraph will be changed later. OOGA BOOGA</p>
                 </div>
               </div>
             </div>
 
-            <div className='row flex' id='projectsrow3'>
-              <div id='project1_div'>
-                <div>
-                  <img src={Project1} alt='Project1 Image' id='project1_img'/>
-                </div>
-                <div className='text-white'>
+            <div className='row flex' id='projectsrow1'>
+              <div className='project_div1'>
+                <div className='text-white p-5'>
                   <p>This is my first project. This paragraph will be changed later. OOGA BOOGA</p>
+                </div>
+                <div>
+                  <img src={Project1} alt='Project3 Image' id='project1_img'/>
                 </div>
               </div>
             </div>
