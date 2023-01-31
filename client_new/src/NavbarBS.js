5fd853dbcd45ac0f0ddda7bc40bb3413935b0132
OzanEFETopcu <ozanefe.topcu@gmail.com>
2023-01-31 21:09:00 +0200

    Changing background color

--- client_new/src/NavbarBS.js
+++ client_new/src/NavbarBS.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import Logo from './Pictures/Logo(inverted).jpg'
 
 export default function NavbarBS(){
   return (
     <>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="black" variant="dark">
           <Container>
+              <Navbar.Brand>
+                <img src={Logo} alt="Logo" width={40} />
+              </Navbar.Brand>
               <Navbar.Brand href="#home">Ozan Topcu</Navbar.Brand>
               <Nav className="me-auto">
                   <Nav.Link href="#home">Home</Nav.Link>
--- client_new/src/Projects.js
+++ client_new/src/Projects.js
@@ -11,33 +11,39 @@ export default function Projects() {
 
             <div className='row flex' id='projectsrow1'>
               <div className='project_div1'>
-                <div className='text-white p-5'>
-                  <p>This is my first project. This paragraph will be changed later. OOGA BOOGA</p>
-                </div>
-                <div>
-                  <img src={Project1} alt='Project1 Image' id='project1_img'/>
+                <div class="layer1">
+                  <div className='text-white p-5'>
+                    <p>This is my first project. This paragraph will be changed later. OOGA BOOGA</p>
+                  </div>
+                  <div>
+                    <img src={Project1} alt='Project1 Image' id='project1_img'/>
+                  </div>
                 </div>
               </div>
             </div>
 
             <div className='row flex' id='projectsrow2'>
               <div className='project_div2'>
-                <div>
-                  <img src={Project1} alt='Project2 Image' id='project2_img'/>
-                </div>
-                <div className='text-white p-5'>
-                  <p>This is my first project. This paragraph will be changed later. OOGA BOOGA</p>
+                <div class="layer2">
+                  <div>
+                    <img src={Project1} alt='Project2 Image' id='project2_img'/>
+                  </div>
+                  <div className='text-white p-5'>
+                    <p>This is my first project. This paragraph will be changed later. OOGA BOOGA</p>
+                  </div>
                 </div>
               </div>
             </div>
 
             <div className='row flex' id='projectsrow1'>
               <div className='project_div1'>
-                <div className='text-white p-5'>
-                  <p>This is my first project. This paragraph will be changed later. OOGA BOOGA</p>
-                </div>
-                <div>
-                  <img src={Project1} alt='Project3 Image' id='project1_img'/>
+                <div class="layer1">
+                  <div className='text-white p-5'>
+                    <p>This is my first project. This paragraph will be changed later. OOGA BOOGA</p>
+                  </div>
+                  <div>
+                    <img src={Project1} alt='Project3 Image' id='project1_img'/>
+                  </div>
                 </div>
               </div>
             </div>
