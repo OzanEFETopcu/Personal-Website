505d2e7139dd22ce1f9c9bf2d02b8a797240087a
OzanEFETopcu <ozanefe.topcu@gmail.com>
2023-02-05 20:38:06 +0200

    Developing the about page

--- client_new/src/About.js
+++ client_new/src/About.js
@@ -9,20 +9,28 @@ export default function About() {
 
           <div className='container-fluid' id='aboutContainer1'>
 
-          <div className='row flex align-items-center justify-content-center' id='about_row1'>
 
-            <div className='col flex-shrink' id='about_col1'>
-                <div className='justify-content-center' id="about_imageDiv">
-                    <img src={AboutImage} alt='Landing Page Pic' id='aboutimg'/>
-                </div>
+            <div style={{ overflow: 'hidden', position:'absolute',
+                          width:'100%', top:'0', bottom:'0', display: 'flex' }}>
+              <div id='background_circle'></div>
             </div>
 
-            <div className='col text-white' id='about_col2'>
-                This is my test landing page. These letters dont mean anything.
-                Typing more so that I can check how it looks on the website.
-                More and more letters are flowing through my website. So how is everybody?
-                Hope this works really good.
-            </div>
+            <div className='row flex align-items-center justify-content-center' id='about_row1'>
+
+
+
+              <div className='col flex-shrink' id='about_col1'>
+                  <div className='justify-content-center' id="about_imageDiv">
+                      <img src={AboutImage} alt='Landing Page Pic' id='aboutimg'/>
+                  </div>
+              </div>
+
+              <div className='col text-dark' id='about_col2'>
+                  This is my test landing page. These letters dont mean anything.
+                  Typing more so that I can check how it looks on the website.
+                  More and more letters are flowing through my website. So how is everybody?
+                  Hope this works really good.
+              </div>
 
             </div>
 
